7198db14c6ed6564eebe66e8f7563a234192e0f6
Davison Tieu <dkhac.tieu@gmail.com>
2020-07-03 22:27:17 -0400

    FEAT: install-universal script for setting up system agnostic config

---
+++ install-universal.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+### --- Neovim --- ###
+
+cp -r --parents .config/nvim ~/
+
+### --- Tmux --- ###
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+cp .tmux.conf ~/.tmux.conf
+
+### --- ZSH & Oh My Zsh --- ###
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Copy zsh related configuration AFTER installing oh-my-zsh to avoid overwriting
+cp .zshrc ~/.zshrc
+
+# Pure prompt
+mkdir -p "$HOME/.zsh"
+git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
+
+# ohmyzsh unofficial plugins
+git clone https://github.com/paulirish/git-open.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/git-open
+git clone https://github.com/lukechilds/zsh-nvm ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-nvm
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
