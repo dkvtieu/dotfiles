e6eef49220b406e4c42df207865859bdd2e5691b
dkvtieu <dkhac.tieu@gmail.com>
2022-02-23 23:23:17 -0500

    FIX: Only run scripts if relevant folders do not exist

--- run_once_before_install_extras.sh
+++ run_once_before_install_extras.sh
@@ -2,20 +2,27 @@
 
 ### --- Tmux --- ###
 
-# NOTE: Remember to install plugins after tmux session is started
-# See https://github.com/tmux-plugins/tpm#installing-plugins
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  # NOTE: Remember to install plugins after tmux session is started
+  # See https://github.com/tmux-plugins/tpm#installing-plugins
+if [ ! -d "$HOME/.tmux" ] ; then
+  mkdir -p ~/.tmux/plugins
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
 
-### --- ZSH & Oh My Zsh --- ###
+### --- ZSH --- ###
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if [ ! -d "$HOME/.zsh" ] ; then
+  # Install oh-my-zsh
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  # Create the .zsh folder
+  mkdir -p "$HOME/.zsh"
+  # Pure prompt
+  git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
 
-# Pure prompt
-mkdir -p "$HOME/.zsh"
-git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
+  # ohmyzsh unofficial plugins
+  git clone https://github.com/paulirish/git-open.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/git-open
+  git clone https://github.com/lukechilds/zsh-nvm ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-nvm
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
 
-# ohmyzsh unofficial plugins
-git clone https://github.com/paulirish/git-open.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/git-open
-git clone https://github.com/lukechilds/zsh-nvm ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-nvm
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
