f64e04677e0efe0da0427afd66d06672ef5b9064
dkvtieu <dkhac.tieu@gmail.com>
2022-08-15 13:25:13 -0400

    FEAT: Catpuccin theme

--- dot_config/nvim/lua/user/colorscheme.lua
+++ dot_config/nvim/lua/user/colorscheme.lua
@@ -1,6 +1,10 @@
+vim.g.catppuccin_flavour = "frappe" -- latte, frappe, macchiato, mocha
+
+require("catppuccin").setup()
+
 vim.cmd([[
 try
-  colorscheme duskfox
+  colorscheme catppuccin
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
--- dot_config/nvim/lua/user/plugins.lua
+++ dot_config/nvim/lua/user/plugins.lua
@@ -70,6 +70,7 @@ return packer.startup(function(use)
     -- Colorschemes
     use("folke/tokyonight.nvim")
     use("EdenEast/nightfox.nvim")
+    use({ "catppuccin/nvim", as = "catppuccin" })
 
     -- cmp plugins
     use("hrsh7th/nvim-cmp") -- The completion plugin
