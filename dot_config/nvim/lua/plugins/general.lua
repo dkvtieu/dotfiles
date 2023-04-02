bf2b3219588b8bc4e84508a36c7e012a78eba100
dkvtieu <dkvtieu@users.noreply.github.com>
2023-04-02 01:00:18 -0400

    FIX: Completion with open buffers + better autocomplete

--- dot_config/nvim/lua/plugins/general.lua
+++ dot_config/nvim/lua/plugins/general.lua
@@ -9,14 +9,6 @@ return {
         check_ts = true, -- treesitter integration
         disable_filetype = { "TelescopePrompt" },
       })
-
-      -- cmp integration
-      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-      local cmp_status_ok, cmp = pcall(require, "cmp")
-      if not cmp_status_ok then
-        return
-      end
-      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({}))
     end,
   },
   { "windwp/nvim-ts-autotag" },
