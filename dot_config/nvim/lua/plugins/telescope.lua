5169c149529c2eb56d9d4acf35c356aab61f95bb
dkvtieu <dkvtieu@users.noreply.github.com>
2023-06-07 11:57:22 -0400

    FEAT: Undo tree telescope

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -3,6 +3,16 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.1",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = { "nvim-lua/plenary.nvim", "debugloop/telescope-undo.nvim" },
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          undo = {
+            -- telescope-undo.nvim config, see below
+          },
+        },
+      })
+      require("telescope").load_extension("undo")
+    end,
   },
 }
