0a07a6a07d55d28b5e8adef6696a02e59c4d369a
dkvtieu <dkvtieu@users.noreply.github.com>
2024-01-12 13:04:37 -0500

    FEAT: More plugins

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -2,17 +2,27 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.1",
+    tag = "0.1.5",
     dependencies = { "nvim-lua/plenary.nvim", "debugloop/telescope-undo.nvim" },
     config = function()
       require("telescope").setup({
         extensions = {
           undo = {
-            -- telescope-undo.nvim config, see below
+            mappings = {
+              i = {
+                ["<C-cr>"] = require("telescope-undo.actions").yank_additions,
+                ["<S-cr>"] = require("telescope-undo.actions").yank_deletions,
+                ["<cr>"] = require("telescope-undo.actions").restore,
+              },
+            },
+          },
+          advanced_git_search = {
+            -- See Config
           },
         },
       })
       require("telescope").load_extension("undo")
+      require("telescope").load_extension("advanced_git_search")
     end,
   },
 }
