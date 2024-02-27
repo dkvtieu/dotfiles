9db64b265274eeb54be0478d4ee96678cedaead3
dkvtieu <dkvtieu@users.noreply.github.com>
2024-02-27 01:21:24 -0500

    FEAT: Better git related keymaps

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -1,19 +1,30 @@
 return {
   {
     "lewis6991/gitsigns.nvim",
-    event = { "BufReadPre", "BufNewFile" },
     opts = {
-      signs = {
-        add = { text = "▎" },
-        change = { text = "▎" },
-        delete = { text = "" },
-        topdelete = { text = "" },
-        changedelete = { text = "▎" },
-        untracked = { text = "▎" },
-      },
+      on_attach = function(buffer)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, desc)
+          vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+        end
+
+        -- stylua: ignore start
+        map("n", "]h", gs.next_hunk, "Next Hunk")
+        map("n", "[h", gs.prev_hunk, "Prev Hunk")
+        map("n", "<leader>gj", gs.next_hunk, "Next Hunk")
+        map("n", "<leader>gk", gs.prev_hunk, "Prev Hunk")
+        map("n", "<leader>gr", gs.reset_hunk, "Reset Hunk")
+        map("n", "<leader>gR", gs.reset_buffer, "Reset Buffer")
+        map("n", "<leader>gp", gs.preview_hunk, "Preview Hunk")
+        map("n", "<leader>gl", function() gs.blame_line({ full = true }) end, "Blame Current Line")
+        map("n", "<leader>gL", function() gs.toggle_current_line_blame({ full = true }) end, "Toggle Blame Line")
+        map("n", "<leader>ghd", gs.diffthis, "Diff This")
+        map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
+        map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
+      end,
     },
   },
-  { "kdheepak/lazygit.nvim" },
   { "akinsho/git-conflict.nvim", config = true },
   {
     "aaronhallaert/advanced-git-search.nvim",
